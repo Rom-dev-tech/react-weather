233f7b00cc3fc0eacd67fd7baeb5f8f508b4d122
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-11-12 11:04:54 +0200

    Preparing to deploy on Netlify

--- src/components/WeatherIcon/WeatherIcon.tsx
+++ src/components/WeatherIcon/WeatherIcon.tsx
@@ -4,7 +4,9 @@ import defaultIcon from '../../images/logo.png';
 export const WeatherIcon = ({ src, alt }: WeatherIconProps) => {
   return (
     <img
-      src={src ? `http://openweathermap.org/img/wn/${src}@2x.png` : defaultIcon}
+      src={
+        src ? `https://openweathermap.org/img/wn/${src}@2x.png` : defaultIcon
+      }
       alt={alt ? alt : 'icon weather'}
     />
   );
