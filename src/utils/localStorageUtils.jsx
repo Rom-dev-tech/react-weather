c93a6d3c63a5e817a5abbc0a6a1429d370433599
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-11-12 10:26:52 +0200

    Remove React project for Pull Request TypeScript project

--- src/utils/localStorageUtils.jsx
+++
@@ -1,5 +0,0 @@
-const DEFAULT_CITY = 'Minsk';
-
-export const setCityToLS = (title) => localStorage.setItem('city', title);
-
-export const getCityFromLS = () => localStorage.getItem('city') || DEFAULT_CITY;
